070bdc365f593f07f3010ef892e1c38c20bef509
jay-juns <necrozerg2@naver.com>
2021-01-12 08:19:53 +0900

    전체 드래그 방지 추가

--- src/App.js
+++ src/App.js
@@ -30,6 +30,9 @@ const App = props => {
     dispatch(checkUserSession());
 
   }, []);
+  window.document.oncontextmenu = new Function("return false"); 
+  window.document.onselectstart = new Function("return false"); 
+  window.document.ondragstart = new Function("return false");
 
   return (
     <div className="App">
