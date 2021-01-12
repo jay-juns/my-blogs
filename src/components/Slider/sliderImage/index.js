44fc69c424c13ece3bdb67fb081e3586a6bf2265
jay-juns <necrozerg2@naver.com>
2021-01-12 14:09:40 +0900

    메인 슬라이더 수정

--- src/components/Slider/sliderImage/index.js
+++ src/components/Slider/sliderImage/index.js
@@ -5,17 +5,17 @@ import image04 from './../../../assets/item/item4.jpg';
 
 export default [
   {
-    title: 'My blogs는 누구나 환영합니다.',
+    title: 'My Blogs는 누구나 환영합니다.',
     description: '편안한 내 집에 온 것처럼 이용하세요.',
     urls: `${image01}`
   },
   {
-    title: 'My blogs는 하루에 지친 당신에게 위로가 됩니다.',
+    title: 'My Blogs는 하루에 지친 당신에게 위로가 됩니다.',
     description: '당신의 상처를 치유 받고 싶듯이 타인에게 또한 배려해주세요.',
     urls: `${image02}`
   },
   {
-    title: 'My blogs는 어떠한 목적없이 도배 및 광고글 작성자는 즉시 추방합니다.',
+    title: 'My Blogs는 어떠한 목적없이 도배 및 광고글 작성자는 즉시 추방합니다.',
     description: '광고, 비방, 욕설, 불법적인 목적으로 글을 작성하시면 영구 추방됩니다.',
     urls: `${image03}`
   },
