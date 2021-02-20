1d338734a8930c9ef00c7e2c22695e8eb644292c
jay-juns <necrozerg2@naver.com>
2021-02-20 17:00:59 +0900

    번역 작업 추가

--- src/components/Slider/sliderData/index.js
+++ src/components/Slider/sliderData/index.js
@@ -5,19 +5,15 @@ import image04 from './../../../assets/item/item4.jpg';
 
 export default  [
     {
-      description: '편안한 내 집에 온 것처럼 이용하세요.',
       urls: `${image01}`
     },
     {
-      description: '타인을 배려해주는 당신이 되었으면 합니다.',
       urls: `${image02}`
     },
     {
-      description: '광고, 비방, 욕설, 불법적인 목적으로 글을 작성하시면 영구 추방됩니다.',
       urls: `${image03}`
     },
     {
-      description: ' 늘 행복하세요.',
       urls: `${image04}`
-    },
+    }
   ]
