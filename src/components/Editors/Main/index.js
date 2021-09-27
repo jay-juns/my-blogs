ffbb1b367718bd4903a37c8999858117e35b9ffe
jay-juns <necrozerg2@naver.com>
2021-09-27 11:05:12 +0900

    test

--- src/components/Editors/Main/index.js
+++ src/components/Editors/Main/index.js
@@ -1,73 +1,59 @@
-import React, { useState } from 'react';
-import ReactCrop from 'react-image-crop';
-import Button from '../../Forms/Button';
-import 'react-image-crop/dist/ReactCrop.css';
-import './styles.scss';
+import React, { useState } from 'react'
+import ImageCropper from '../ImageCropper'
+import Button from '../../Forms/Button'
+import './styles.scss'
 
 const EditorMain = () => {
-  const [src, selectFile] = useState(null);
-  const [image, setImage] = useState(null);
-  const [crop, setCrop] = useState({ aspect: 1 / 1 });
-  const [result, setResult] = useState(null);
-  
-  const handleFileChange = e => {
-    selectFile(URL.createObjectURL(e.target.files[0]))
+  const [blob, setBlob] = useState(null)
+  const [inputImg, setInputImg] = useState('')
+
+  const getBlob = (blob) => {
+      setBlob(blob)
   }
 
-  function getCroppedImg() {
-    const canvas = document.createElement('canvas');
-    const scaleX = image.naturalWidth / image.width;
-    const scaleY =image.naturalHeight / image.height;
-    canvas.width = crop.width;
-    canvas.height = crop.height;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(
-      image,
-      crop.x * scaleX,
-      crop.y * scaleY,
-      crop.width * scaleX,
-      crop.height * scaleY,
-      0,
-      0,
-      crop.width*scaleX,
-      crop.height*scaleY,
-    );
-    
-    const base64Image = canvas.toDataURL('image/jpg');
-    setResult(base64Image);
+  const onInputChange = (e) => {
+      // convert image file to base64 string
+      const file = e.target.files[0]
+      const reader = new FileReader()
+
+      reader.addEventListener('load', () => {
+          setInputImg(reader.result)
+      }, false)
+
+      if (file) {
+          reader.readAsDataURL(file)
+      }
+  }
+
+  const handleSubmitImage = (e) => {
+      e.preventDefault()
+      console.log(blob);
   }
 
   return (
-    <div className="editor--main">
+    <>
+    <div style={{display: 'flex', flexDirection: 'column'}}>
       <div>
-        <input type='file' accet='image/*' onChange={handleFileChange} />
+        <form onSubmit={handleSubmitImage}>
+          <input
+            type='file'
+            accept='image/*'
+            onChange={onInputChange}
+          />
+          {
+            inputImg && (
+              <ImageCropper
+                  getBlob={getBlob}
+                  inputImg={inputImg}
+              />
+            )
+          }
+          <Button className="btn save-image-btn" type='submit'>보내기</Button>
+        </form>
       </div>
-      <div className="editor--show-area">
-        {src && (
-          <div className="editor--show-area-contents">
-            <ReactCrop
-              src={src} 
-              onImageLoaded={setImage}
-              crop={crop}
-              onChange={setCrop}
-              className="editor--show-area-img-wrapper"
-            />
-            <Button 
-              className="btn cut-btn"
-              onClick={getCroppedImg}
-            >
-              이미지 자르기
-            </Button>
-          </div>
-        )}
-      </div>
-      {result && (
-        <div className="editor--show-cut-area">
-          <img src={result} alt="image" />
-        </div>
-      )}
     </div>
-  );
+    </>
+  )
 };
 
 export default EditorMain;
